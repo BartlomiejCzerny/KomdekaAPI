e0d2c32b746bc2129a949048bea4900b2b7eff45
Bartłomiej Czerny <bartlomiejczerny@gmail.com>
2022-11-17 20:04:30 +0100

    Obsługa błędów i refaktoryzacja kodu

--- KomdekaAPI/Entities/DataTransferObjects/ToolDto.cs
+++ KomdekaAPI/Entities/DataTransferObjects/ToolDto.cs
@@ -7,20 +7,6 @@ namespace KomdekaAPI.Entities.DataTransferObjects
 {
     public class ToolDto
     {
-        //public string IdNumber { get; set; }
-
-        //public string Name { get; set; }
-
-        //public string Type { get; set; }
-
-        //public string SerialNumber { get; set; }
-
-        //public bool IsMetrologicalService { get; set; }
-
-        //public DateTime PeriodValidity { get; set; }
-
-        //public string MetrologicalServiceInterval { get; set; }
-
-        //public string Status { get; set; }
+        public string ErrorMessage { get; set; }
     }
 }
