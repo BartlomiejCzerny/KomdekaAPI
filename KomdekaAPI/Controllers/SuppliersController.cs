bc3e9177b34f9070572997710d0089529c0ec6a8
Bartłomiej Czerny <bartlomiejczerny@gmail.com>
2022-11-23 19:44:22 +0100

    Dodanie API panelu dostawców

---
+++ KomdekaAPI/Controllers/SuppliersController.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using KomdekaAPI.Entities;
+using KomdekaAPI.Entities.Models;
+
+namespace KomdekaAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SuppliersController : ControllerBase
+    {
+        private readonly RepositoryContext _context;
+
+        public SuppliersController(RepositoryContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Supplier>>> GetSupplier()
+        {
+            return await _context.Supplier.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Supplier>> GetSupplier(string id)
+        {
+            var supplier = await _context.Supplier.FindAsync(id);
+
+            if (supplier == null)
+            {
+                return NotFound("Nie znaleziono dostawcy.");
+            }
+
+            return supplier;
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutSupplier(string id, Supplier supplier)
+        {
+            if (id != supplier.IdNumber)
+            {
+                return BadRequest("Nie można zmienić numeru identyfikacyjnego dostawcy.");
+            }
+
+            _context.Entry(supplier).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!SupplierExists(id))
+                {
+                    return NotFound("Nie znaleziono dostawcy.");
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Supplier>> PostSupplier(Supplier supplier)
+        {
+            _context.Supplier.Add(supplier);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (SupplierExists(supplier.IdNumber))
+                {
+                    return BadRequest("Dostawca o podanym numerze identyfikacyjnym istnieje.");
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtAction("GetSupplier", new { id = supplier.IdNumber }, supplier);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteSupplier(string id)
+        {
+            var supplier = await _context.Supplier.FindAsync(id);
+            if (supplier == null)
+            {
+                return NotFound("Nie znaleziono dostawcy.");
+            }
+
+            _context.Supplier.Remove(supplier);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool SupplierExists(string id)
+        {
+            return _context.Supplier.Any(e => e.IdNumber == id);
+        }
+    }
+}
---
+++ KomdekaAPI/Migrations/20221119223119_SuppliersTableCreation.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace KomdekaAPI.Migrations
+{
+    public partial class SuppliersTableCreation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Supplier",
+                columns: table => new
+                {
+                    IdNumber = table.Column<string>(type: "nvarchar(15)", maxLength: 15, nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: false),
+                    Street = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: true),
+                    BuildingNumber = table.Column<string>(type: "nvarchar(15)", maxLength: 15, nullable: false),
+                    ZipCode = table.Column<string>(type: "nvarchar(6)", maxLength: 6, nullable: false),
+                    Place = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: false),
+                    ActivitiesRange = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: false),
+                    ApprovalDate = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: false),
+                    ApprovalExpirationDate = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: false),
+                    Remarks = table.Column<string>(type: "nvarchar(max)", maxLength: 5000, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Supplier", x => x.IdNumber);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Supplier");
+        }
+    }
+}
