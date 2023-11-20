31f97accc9f84242d836146b89c971f6012a6b06
Bartłomiej Czerny <bartlomiejczerny@gmail.com>
2023-11-20 13:25:22 +0100

    Migracja do .NET 8

--- EmailService/Message.cs
+++ EmailService/Message.cs
@@ -12,9 +12,7 @@ namespace EmailService
 
         public Message(IEnumerable<string> to, string subject, string content)
         {
-            To = new List<MailboxAddress>();
-
-            To.AddRange(to.Select(x => new MailboxAddress(x)));
+            To = [.. to.Select(x => new MailboxAddress(string.Empty, x))];
             Subject = subject;
             Content = content;
         }
