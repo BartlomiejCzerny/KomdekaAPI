ca668cbf2211a90a5c5ecafdb8b4bfdfa853c94f
Bartłomiej Czerny <bartlomiejczerny@gmail.com>
2024-03-25 20:06:17 +0100

    Aktualizacja pakietów NuGet do najnowszej wersji

--- EmailService/obj/Debug/net8.0/EmailService.AssemblyInfo.cs
+++ EmailService/obj/Debug/net8.0/EmailService.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EmailService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55e038925db64c7eb9744f4980c47da94901a91e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c9bac18fe2bc9a6507546d44bb908c19a74e9bb")]
 [assembly: System.Reflection.AssemblyProductAttribute("EmailService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EmailService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
